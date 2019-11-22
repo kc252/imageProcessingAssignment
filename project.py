7b933d33614d22048307bed8740fc37180c06144
SatansCat <endamkeane@gmail.com>
2019-11-22 10:36:54 +0000

    UI changes testing

--- project.py
+++ project.py
@@ -3,8 +3,11 @@ import sys
 import easygui
 import numpy as np
 from matplotlib import pyplot as plt
+import tkinter as tk
+from tkinter import*
 
-def char_generator():
+
+"""def char_generator():
   n = str(input("Enter a message to hide: "))
   for c in n:
     yield ord(c)
@@ -52,7 +55,58 @@ def main():
   #print "Image Encoded Sucessfully"
   #cv2.imwrite("output.jpeg", img)
 
-  #print decode_image()
+  print(decode_image())
+
 
 if __name__ == "__main__":
-	main()
+    main()
+"""
+
+
+class SampleApp(tk.Tk):
+  def __init__(self):
+    tk.Tk.__init__(self)
+    self._frame = None
+    self.switch_frame(StartPage)
+
+
+  def switch_frame(self, frame_class):
+    new_frame = frame_class(self)
+
+    if self._frame is not None:
+      self._frame.destroy()
+    self._frame = new_frame
+    self._frame.pack()
+
+
+class StartPage(tk.Frame):
+    def __init__(self, master):
+      tk.Frame.__init__(self, master)
+
+      label = tk.Label(self, text="Please choose a function", bg='dark gray', font=("Times", 20, 'bold'))
+      label.pack(side=RIGHT, expand=True)
+      #tk.Label(self, text="Start page", font=('Times', 18, "bold")).pack(side="top", fill="x", pady=5)
+      Button1 = tk.Button(self, text="Go to page one",
+                command=lambda: master.switch_frame(PageOne)).place(x=0,y=10)
+      #Button1.place(x=0,y=10)
+      #tk.Button(self, text="Go to page two",
+      #          command=lambda: master.switch_frame(PageTwo)).pack()
+
+
+class PageOne(tk.Frame):
+  def __init__(self, master):
+    tk.Frame.__init__(self, master)
+    tk.Frame.configure(self, bg='blue')
+    tk.Label(self, text="Page one", font=('Helvetica', 18, "bold")).pack(side="left", fill="x", pady=5)
+    tk.Button(self, text="Go back to start page",
+              command=lambda: master.switch_frame(StartPage)).pack()
+
+
+
+
+if __name__ == "__main__":
+    window = SampleApp()
+    window.geometry("1000x500")
+    window.title("Steganography Assignment")
+    window.config(background='Dark gray')
+    window.mainloop()
