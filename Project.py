ace2db684370c37a6b1b83d9b712d33eb82cafe0
Kevin <kev.camp252@gmail.com>
2019-11-25 03:35:14 +0000

    pretty much done

--- Project.py
+++ Project.py
@@ -1,4 +1,3 @@
-
 import cv2
 import easygui
 from easygui import choicebox, enterbox, textbox, msgbox
@@ -104,12 +103,12 @@ def decodeImageFromImage(i1):
 # in = colourCover, colourHidden; out = colourEncoded
 def encodeBits(coverVal, hiddenVal):
     # convert the cover to binary
-    #print("##########")
+    # print("##########")
     ##print(hiddenVal)
     redBinCover = '{0:08b}'.format(coverVal[0])
     greenBinCover = '{0:08b}'.format(coverVal[1])
     blueBinCover = '{0:08b}'.format(coverVal[2])
-    #print(blueBinCover)
+    # print(blueBinCover)
     # cut off the end 2 characters
     redBinCover = redBinCover[:-2]
     greenBinCover = greenBinCover[:-2]
@@ -119,13 +118,13 @@ def encodeBits(coverVal, hiddenVal):
     redBinHidden = '{0:08b}'.format(hiddenVal[0])
     greenBinHidden = '{0:08b}'.format(hiddenVal[1])
     blueBinHidden = '{0:08b}'.format(hiddenVal[2])
-    #print("ToHide: "+blueBinHidden)
+    # print("ToHide: "+blueBinHidden)
 
     # Add one to the end of the other.
     redBinCover = redBinCover + redBinHidden[:2]
     greenBinCover = greenBinCover + greenBinHidden[:2]
     blueBinCover = blueBinCover + blueBinHidden[:2]
-    #print(blueBinCover)
+    # print(blueBinCover)
     # print(int(redBinCover, 2), int(greenBinCover, 2), int(blueBinCover, 2))
 
     return int(redBinCover, 2), int(greenBinCover, 2), int(blueBinCover, 2)
@@ -135,19 +134,19 @@ def decodeBits(inputVal):
     redBin = '{0:08b}'.format(inputVal[0])
     greenBin = '{0:08b}'.format(inputVal[1])
     blueBin = '{0:08b}'.format(inputVal[2])
-    #print("Blue: " + blueBin)
-    #print("Green: " + greenBin)
-    #print("Red: " + redBin)
+    # print("Blue: " + blueBin)
+    # print("Green: " + greenBin)
+    # print("Red: " + redBin)
 
     print(redBin[-2:] + " 000000")
     redBin = redBin[-2:] + '000000'
     greenBin = greenBin[-2:] + '000000'
     blueBin = blueBin[-2:] + '000000'
 
-
     return int(redBin, 2), int(greenBin, 2), int(blueBin, 2)
 
-#ATTEMPTED GUI CODE
+
+# ATTEMPTED GUI CODE
 
 # class SampleApp(tk.Tk):
 #     def __init__(self):
@@ -243,25 +242,52 @@ def decodeBits(inputVal):
 if __name__ == "__main__":
 
     # encode
-
     msg = "Do you want to encode or decode and Image"
     title = "Please Confirm"
     choices = ["Encode", "Decode"]
     choice = choicebox(msg, title, choices)
 
     if choice == "Encode":
-        message = enterbox("Enter secret message: ")
-        msgbox("Please choose image to encode:")
-        img = encode_image(message)
-        print("Image Encoded Successfully")
-        cv2.imwrite("output.png", img)
+        msg = "Do you want to encode text or an image"
+        title = "Please Confirm"
+        choices = ["Image", "Text"]
+        choice = choicebox(msg, title, choices)
+
+        if choice == "Text":
+            message = enterbox("Enter secret message: ")
+            msgbox("Please choose image to encode:")
+            img = encode_image(message)
+            print "Image Encoded Successfully"
+            cv2.imwrite("output.png", img)
+
+        elif choice == "Image":
+            msgbox("Please choose image to hide:")
+            i2 = get_image()
+            msgbox("Please choose image to cover:")
+            i1 = get_image()
+            img = encodeImageToImage(i1, i2)
+            print "Image Decoded Successfully"
+            cv2.imwrite("output2.png", img)
 
     elif choice == "Decode":
-        msgbox("Please choose image to decode:")
-        decodedString = decode_image()
-        msgbox("Secret Message = " + decodedString)
+        msg = "Decode image from image or text from image: "
+        title = "Please Confirm"
+        choices = ["Image", "Text"]
+        choice = choicebox(msg, title, choices)
 
-    # # decode
+        if choice == "Text":
+            msgbox("Choose image to decode: ")
+            decodedString = decode_image()
+            msgbox("Secret Message = " + decodedString)
+
+        elif choice == "Image":
+            msgbox("Choose image to decode: ")
+            image = get_image()
+            decodedImage = decodeImageFromImage(image)
+            cv2.imwrite("decodedImage.png",decodedImage)
+            print "Image Decoded Successfully"
+
+    # decode
     # print(decode_image())
 
     # img = get_image()
@@ -283,4 +309,3 @@ if __name__ == "__main__":
     # window.config(background='Light gray')
     # window.rowconfigure(0, weight=1)
     # window.mainloop()
-
