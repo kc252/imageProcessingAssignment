93b186911a199c540a7f9283b73b1bab2ed96da5
SatansCat <endamkeane@gmail.com>
2019-11-25 00:12:16 +0000

    Working on image hiding

--- Project.py
+++ Project.py
@@ -17,7 +17,12 @@ def char_generator():
 def get_image():
     f = easygui.fileopenbox()
     I = cv2.imread(f)
-    return I
+    try:
+        rgbImg = cv2.cvtColor(I, cv2.COLOR_BGR2RGB)
+    except cv2.error:
+        print("Incorrect input type, please select an image of appropriate type")
+        exit(1)
+    return rgbImg
 
 
 def gcd(a, b):
@@ -53,6 +58,77 @@ def decode_image():
                 else:
                     return message
 
+
+# encodeImageToImage(cover,hidden)
+def encodeImageToImage(i1, i2):
+    x1, y1, z1 = i1.shape
+    x2, y2, x2 = i2.shape
+    i3 = i1
+    if x1 < x2 or y1 < y2:
+        print("Incorrect size")
+        print(i1.size)
+        print(i2.size)
+    else:
+        print("Correct size")
+        #print("Size is X:"+x1+" Y:"+y1)
+        for i in range(x1):
+            for j in range(y1):
+                if i > x2 or j > y2:
+                    #print("Outside bounds of Hidden, painting black")
+                    i3[i, j] = encodeBits(i1[i, j], (0, 0, 0))
+                else:
+                    i3[i, j] = encodeBits(i1[i, j], i2[i, j])
+                #print(i3[i, j])
+        return i3
+
+def decodeImageFromImage(i1):
+    i2 = i1
+    x1, y1, z1 = i1.shape
+    for i in range(x1):
+        for j in range(y1):
+            i2[i, j] = decodeBits(i1[i, j])
+    return i2
+
+#in = colourCover, colourHidden; out = colourEncoded
+def encodeBits(coverVal, hiddenVal):
+    # convert the cover to binary
+    print("##########")
+    print(coverVal)
+    redBinCover = format(coverVal[0], '08b')
+    greenBinCover = format(coverVal[1], '08b')
+    blueBinCover = format(coverVal[2], '08b')
+    print(redBinCover)
+    # cut off the end 2 characters
+    redBinCover = redBinCover[:-2]
+    greenBinCover = greenBinCover[:-2]
+    blueBinCover = blueBinCover[:-2]
+    print(redBinCover)
+
+    redBinHidden = format(hiddenVal[0], '08b')
+    greenBinHidden = format(hiddenVal[1], '08b')
+    blueBinHidden = format(hiddenVal[2], '08b')
+    print(redBinHidden[-2:])
+
+    # Add one to the end of the other.
+    redBinCover = redBinCover + redBinHidden[-2:]
+    greenBinCover = greenBinCover + greenBinHidden[-2:]
+    blueBinCover = blueBinCover + blueBinHidden[-2:]
+    #print(redBinCover)
+
+    return (int(redBinCover, 2), int(greenBinCover, 2), int(blueBinCover, 2))
+
+
+def decodeBits(inputVal):
+    redBin = format(inputVal[0], '08b')
+    greenBin = format(inputVal[1], '08b')
+    blueBin = format(inputVal[2], '08b')
+
+    #print(redBin + " " + redBin[:-2])
+    redBin = redBin[:-2] + '000000'
+    greenBin = greenBin[:-2] + '000000'
+    blueBin = blueBin[:-2] + '000000'
+
+    return (int(redBin, 2), int(greenBin, 2), int(blueBin, 2))
 
 class SampleApp(tk.Tk):
     def __init__(self):
@@ -155,6 +231,21 @@ if __name__ == "__main__":
 
     # decode
     # print(decode_image())
+
+
+
+    img = get_image()
+    img2 = get_image()
+    img3 = encodeImageToImage(img,img2)
+    img4 = decodeImageFromImage(img3)
+
+    plt.imshow(img3)
+    plt.imshow(img4)
+    plt.show()
+
+    print(img.size)
+    x, y, z = img.shape
+    print(x*y*z)
 
     window = SampleApp()
     window.geometry("1000x500")
